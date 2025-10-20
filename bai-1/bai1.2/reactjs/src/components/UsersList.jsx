2f10952c61537d4e3b8be16f7e19f3ccb20703ed
caothienqui1801 <caothienqui1801@gmail.com>
2025-10-20 22:57:18 +0700

    update

--- bai-1/bai1.2/reactjs/src/components/UsersList.jsx
+++ bai-1/bai1.2/reactjs/src/components/UsersList.jsx
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 export default function UserList() {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     axios
@@ -18,18 +19,40 @@ export default function UserList() {
     return <h3>Loading...</h3>;
   }
 
+  const filteredUsers = users.filter(u =>
+    u.name.toLowerCase().includes(search.toLowerCase())
+  );
+
 
   return (
-    <ul>
-      {users.map((u, index) => (
-        <li key={u.id}>
-          {/* Click vào tên user dể xem chi tiết! */}
-          <Link to={`/users/${u.id}`}>
-            {index + 1} {u.name}
-          </Link>{" "}
-          - {u.email}
-        </li>
-      ))}
-    </ul>
+    <div>
+      <h2>Danh sách người dùng</h2>
+
+      {/*Ô nhập tìm kiếm*/}
+
+      <input
+        type="text"
+        placeholder="Tìm kiếm tên user tại đây!"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        style={{ marginBottom: 10, padding: 5 }}
+      />
+
+      {/* Danh sách kết quả */}
+      <ul>
+        {filteredUsers.length > 0 ? (
+          filteredUsers.map((u, index) => (
+            <li key={u.id}>
+              <Link to={`/users/${u.id}`}>
+                {index + 1} {u.name}
+              </Link>{""}
+              -{u.email}
+            </li>
+          ))
+        ) : (
+          <p>Không tìm thấy người dùng!!</p>
+        )}
+      </ul>
+    </div>
   );
 }
